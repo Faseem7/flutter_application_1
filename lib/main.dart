10b4ae5fcf8f2255636feb10387c164e7754a44c
Faseem7 <76933640+Faseem7@users.noreply.github.com>
2023-09-07 20:58:18 +0530

    completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,70 +11,98 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return const MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Color.fromARGB(255, 0, 0, 0),
         body: SafeArea(
-          child: Column(children: [
-            const CircleAvatar(
-              radius: 60,
-              backgroundImage: AssetImage('images/img1.jpg'),
-            ),
-            const Text(
-              "Faseem",
-              style: TextStyle(
-                  fontFamily: 'KaushanScript-Regular',
-                  fontSize: 23,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 60,
+                backgroundImage: AssetImage('images/img1.jpg'),
+              ),
+              Text(
+                "Faseem",
+                style: TextStyle(
+                    fontFamily: 'KaushanScript-Regular',
+                    fontSize: 23,
+                    color: Color.fromARGB(255, 24, 128, 197),
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.5),
+              ),
+              Text(
+                "Student",
+                style: TextStyle(
+                  fontSize: 25,
+                  fontFamily: 'AmaticSC-Bold',
                   color: Colors.white,
                   fontWeight: FontWeight.bold,
-                  letterSpacing: 2.5
+                ),
+              ),
+              SizedBox(
+                height: 40,
+                width: 150,
+                child:
+                Divider(
+                  color: Colors.white,
+                ) ,
+              ),
+              Card(
+                color: Color.fromARGB(255, 24, 128, 197),
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 10,
+                ),
+                
+                child: ListTile(
+                  leading:  Icon(
+                      Icons.phone_android,
+                      size: 35,
+                      color: Color.fromARGB(255, 0, 0, 0),
+                    ),
+                    title: Text(
+                      "+91 9846672172",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontFamily: 'PermanentMarker-Regular',
+                        color: Color.fromARGB(255, 0, 0, 0),
+                      ),
+                    ),
+                )
+              ),
+              SizedBox(
+                height: 1,
+              ),
+              Card(
+                color: Color.fromARGB(255, 24, 128, 197),
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 10,
+                ),
+               
+                child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      size: 35,
+                      color: Color.fromARGB(255, 0, 0, 0),
+                    ),
+                    title: Text(
+                    "mohdfaseem7@gmail.com",
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Caveat-VariableFont_wght',
+                      color: Color.fromARGB(255, 0, 0, 0),
+                    ),
                   ),
-                  
-            ),
-            const Text(
-              "Student",
-              style: TextStyle(
-                fontSize: 25,
-                fontFamily: 'AmaticSC-Bold',
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-              ), 
-            ),
-         // ignore: avoid_unnecessary_containers
-         Container(
-            color: Colors.white,
-            
-            child:const Row(children: [
-              Icon(Icons.phone_android,
-                    size: 35,
-                  ),
-                Text("+91 9846672172",style: TextStyle(
-                  fontSize: 20,
-                  
-                   fontFamily: 'PermanentMarker-Regular'
-                ),)
-               ],),
+                ), 
+              )
+            ],
           ),
-         const SizedBox(height: 20,),
-          Container(
-            color: Colors.white,
-            child: const Row(
-              children: [
-                Icon(Icons.email,
-                size:35,),
-              Text("mohdfaseem7@gmail.com",
-              style: TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
-                fontFamily: 'Caveat-VariableFont_wght'
-              ),)
-              ],
-            
-            ),
-          )
-          ],),
         ),
       ),
     );
   }
 }
+ 
